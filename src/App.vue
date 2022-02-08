5c1985b5d2a2893e553ee11a09a41ded06e0f9e2
Nic Gerhardt <nicolas.gerhardt@gmail.com>
2022-02-08 16:54:43 -0500

    base project

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
+  mounted() {
+    console.log(`the inital count is: ${this.count}`);
+  },
+};
+</script>
+
+<template>
+  <h1>This is my Vue site testing gh-pages</h1>
+  <h2>
+    <img src="./assets/me.png" alt="profile" />
+    By: Nic G
+  </h2>
+  <button @click="increment">Count is: {{ count }}</button>
+</template>
+
+<style>
+button {
+  font-size: 2em;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+}
+
+img {
+  width: 100px;
+  height: 100px;
+}
+</style>
